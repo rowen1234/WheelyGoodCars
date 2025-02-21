cda3544eb7565ec824b17086fef7b46568ef697a
Rowen Bieshaar <rowen.04@hotmail.com>
2025-02-21 10:12:22 +0100

    add relation to cars model

--- app/Models/Cars.php
+++ app/Models/Cars.php
@@ -8,5 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Cars extends Model
 {
     use HasFactory;
-    protected $fillable = ['license_plate', 'make', 'model', 'price', 'mileage', 'seats', 'doors', 'production_year', 'weight', 'color', 'image', 'sold_at', 'views'];
+    protected $fillable = [
+        'license_plate',
+        'make',
+        'model',
+        'price',
+        'mileage',
+        'seats',
+        'doors',
+        'production_year',
+        'weight',
+        'color',
+        'image',
+        'sold_at',
+        'views'
+    ];
+
+    function cartag()
+    {
+        $this->belongsTo(CarTag::class);
+    }
 }
